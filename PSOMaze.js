9e88cae65a9141b33c81cfc37b9a6740035995a1
Yousuf Uyghur <yousufuighur@yahoo.com>
2024-04-02 22:33:41 +0500

    removed redundancies, refactored and fixed some parts

--- PSOMaze.js
+++ PSOMaze.js
@@ -28,8 +28,8 @@ let current;
 let stack = [];
 const particles = [];
 let noOfParticles = 1000;
-let speed = 1;
-let last; 
+let speed = 3;
+let last;
 // buttons and sliders
 let newMazeButton, startButton, pauseButton, resetButton;
 let particleSlider, speedSlider;
@@ -38,72 +38,56 @@ let elapsedTime = "00:00:00";
 let time = 0;
 let interval;
 
-function styleButton(button) {
-    button.style('background-color', '#4CAF50');
-    button.style('padding', '10px 20px');
-    button.style('font-size', '16px');
-    button.style('border', 'none');
-    button.style('border-radius', '5px');
-}
-
-function toggleButtonAttribute(button, attribute, value) {
-    if (value) {
-        button.removeAttribute(attribute);
-    } else {
-        button.attribute(attribute, true);
+class Particle {
+    constructor() {
+        this.radius = 10;
+        // the ball will appear in the first cell, however, it will be random between 1 - 50 at what pixel it appears 
+        this.position = createVector(grid[0].i * size + random(size - 10), grid[0].j * size + random(size - 10)); // position of the particles, will always start with the first cell
+        this.velocity = p5.Vector.random2D(); // new 2D unit vector with a random heading.
     }
-}
 
-// Function to check if any particle has reached the last cell
-function checkMazeCompletion() {
-    for (var i = 0; i < particles.length; i++) {
-        var particle = particles[i];
-        // Calculate the position of the particle in terms of grid cells
-        var particleColumn = Math.floor(particle.position.x / size);
-        var particleRow = Math.floor(particle.position.y / size);
-        
-        // Check if the particle has reached the last cell
-        if (particleColumn === cols - 1 && particleRow === rows - 1) {
-            pauseMaze(); // Pause the maze
-            stopTimer(); // Stop the timer
-            var completionTime = elapsedTime;
-            var completionParticles = particles.length;
-            var completionSpeed = speed;
-            alert("Maze completed!\nTime: " + completionTime + "\nParticles: " + completionParticles + "\nSpeed: " + completionSpeed + "\nRefresh to Restart.");
-            break;
+    show() { // style of the ellipse
+        stroke(0, 150);
+        fill(112, 183, 126);
+        ellipse(this.position.x, this.position.y, this.radius);
+    }
+
+    move() {
+        this.position.add(this.velocity); // continuously adds the change in position to the position vector 
+    }
+
+    handleCollision() {
+        for (var gridElement = 0; gridElement < grid.length; gridElement++) {
+            var cell = grid[gridElement];
+            if (this.position.x > cell.i * size && this.position.x < (cell.i + 1) * size &&
+                this.position.y > cell.j * size && this.position.y < (cell.j + 1) * size) {
+                // Particle is inside the cell
+                if (cell.walls[0] && this.position.y - this.radius < cell.j * size) {
+                    // Top wall collision
+                    this.velocity.y *= -1; // Reverse the y velocity
+                    this.position.y = cell.j * size + this.radius; // Adjust position to prevent overlap
+                }
+                if (cell.walls[1] && this.position.x + this.radius > (cell.i + 1) * size) {
+                    // Right wall collision
+                    this.velocity.x *= -1; // Reverse the x velocity
+                    this.position.x = (cell.i + 1) * size - this.radius; // Adjust position to prevent overlap
+                }
+                if (cell.walls[2] && this.position.y + this.radius > (cell.j + 1) * size) {
+                    // Bottom wall collision
+                    this.velocity.y *= -1; // Reverse the y velocity
+                    this.position.y = (cell.j + 1) * size - this.radius; // Adjust position to prevent overlap
+                }
+                if (cell.walls[3] && this.position.x - this.radius < cell.i * size) {
+                    // Left wall collision
+                    this.velocity.x *= -1; // Reverse the x velocity
+                    this.position.x = cell.i * size + this.radius; // Adjust position to prevent overlap
+                }
+            }
         }
     }
 }
 
-// function to start the stopwatch 
-function startTimer() {
-    if (interval) {
-        clearInterval(interval);
-    }
 
-    // sets the time 
-    interval = setInterval(() => {
-        time += 1
-        elapsedTime =
-            Math.floor(time / 3600).toString().padStart(2, "0") + ":" + Math.floor((time % 3600) / 60).toString().padStart(2, "0") + ":" + Math.floor((time % 60)).toString().padStart(2, "0")
-    }, 1000);
-}
-
-// stops and clear the timer 
-function stopTimer() {
-    clearInterval(interval);
-    interval = null;
-}
-
-// Function to calculate the index of a cell in the grid array
-function index(column, row) {
-    // Check if the column or row is out of bounds
-    if (column < 0 || row < 0 || column > cols - 1 || row > rows - 1) {
-        return -1;
-    }
-    // Return the calculated index
-    return column + row * cols;
-}
 // Cell class representing each cell in the grid
 class Cell {
     constructor(i, j) {
@@ -159,6 +143,58 @@ class Cell {
     }
 }
 
+// Function to calculate the index of a cell in the grid array
+function index(column, row) {
+    // Check if the column or row is out of bounds
+    if (column < 0 || row < 0 || column > cols - 1 || row > rows - 1) {
+        return -1;
+    }
+    // Return the calculated index
+    return column + row * cols;
+}
+
+// Function to check if any particle has reached the last cell
+function checkMazeCompletion() {
+    for (var i = 0; i < particles.length; i++) {
+        var particle = particles[i];
+        // Calculate the position of the particle in terms of grid cells
+        var particleColumn = Math.floor(particle.position.x / size);
+        var particleRow = Math.floor(particle.position.y / size);
+
+        // Check if the particle has reached the last cell
+        if (particleColumn === cols - 1 && particleRow === rows - 1) {
+            pauseMaze(); // Pause the maze
+            stopTimer(); // Stop the timer
+            var completionTime = elapsedTime;
+            var completionParticles = particles.length;
+            var completionSpeed = speed;
+            alert("Maze completed!\nTime: " + completionTime + "\nParticles: " + completionParticles + "\nSpeed: " + completionSpeed + "\nRefresh to Restart.");
+            break;
+        }
+    }
+}
+
+// function to start the stopwatch 
+function startTimer() {
+    if (interval) {
+        clearInterval(interval);
+    }
+
+    // sets the time 
+    interval = setInterval(() => {
+        time += 1
+        elapsedTime =
+            Math.floor(time / 3600).toString().padStart(2, "0") + ":" + Math.floor((time % 3600) / 60).toString().padStart(2, "0") + ":" + Math.floor((time % 60)).toString().padStart(2, "0")
+    }, 1000);
+}
+
+// stops and clear the timer 
+function stopTimer() {
+    clearInterval(interval);
+    interval = null;
+}
+
+
 // Function to remove wall between current cell and neighboring cell to generate maze
 function removeWall(current, neighbour) {
     // Calculate the difference between the row and column indices of the neighboring cell and the current cell
@@ -183,6 +219,22 @@ function removeWall(current, neighbour) {
     }
 }
 
+function createCells() {
+    for (var row = 0; row < rows; row++) {
+        for (var column = 0; column < cols; column++) {
+            var cell = new Cell(column, row);
+            grid.push(cell);
+        }
+    }
+}
+
+function styleButton(button) {
+            button.style('background-color', '#4CAF50');
+            button.style('padding', '10px 20px');
+            button.style('font-size', '16px');
+            button.style('border', 'none');
+            button.style('border-radius', '5px');
+}
 
 // Setup function to initialize the canvas and grid
 function setup() {
@@ -193,49 +245,44 @@ function setup() {
     frameRate(120);
 
     document.getElementById("time-heading").textContent = "ELAPSED TIME: " + elapsedTime;
+    document.getElementById("frame-rate").textContent = "FPS: " + Math.floor(frameRate());
 
     // Create cells and add them to the grid
-    for (var row = 0; row < rows; row++) {
-        for (var column = 0; column < cols; column++) {
-            var cell = new Cell(column, row);
-            grid.push(cell);
-        }
-    }
+    createCells();
 
     generateMaze();
 
-
     // Create the "Generate New Maze" button
     newMazeButton = createButton('Generate New Maze');
-    newMazeButton.position(1150, 200);
+    newMazeButton.position(BUTTON_X_POSITION, BUTTON_Y_POSITIONS[0]);
     newMazeButton.elt.addEventListener('click', generateNewMaze);
     styleButton(newMazeButton);
 
     // Create the "Start" button
     startButton = createButton("Start the Maze");
-    startButton.position(1150, 250);
+    startButton.position(BUTTON_X_POSITION, BUTTON_Y_POSITIONS[1]);
     startButton.elt.addEventListener('click', startMaze);
     styleButton(startButton);
 
     // Create the "Pause" button
     pauseButton = createButton("Pause the Maze");
-    pauseButton.position(1150, 300);
+    pauseButton.position(BUTTON_X_POSITION, BUTTON_Y_POSITIONS[2]);
     pauseButton.elt.addEventListener('click', pauseMaze);
     pauseButton.attribute('disabled', true);
     styleButton(pauseButton);
 
     // Create the "Stop" button
     resetButton = createButton("Reset the Maze");
-    resetButton.position(1150, 350);
+    resetButton.position(BUTTON_X_POSITION, BUTTON_Y_POSITIONS[3]);
     resetButton.elt.addEventListener('click', resetMaze);
     resetButton.attribute('disabled', true);
     styleButton(resetButton);
 
     // paramters: min, max, initial value, step
     // Create the "Number of Particles" slider
-    particleSlider = createSlider(300, 10000, noOfParticles, 50);
+    particleSlider = createSlider(PARTICLE_MIN, PARTICLE_MAX, noOfParticles, PARTICLE_STEP);
     noOfParticles = particleSlider.value();
-    particleSlider.position(1450, 200);
+    particleSlider.position(SLIDER_X_POSITION, 200);
     particleSlider.size(200);
     // Function to create no of particles based on the slider value
     particleSlider.input(() => {
@@ -243,9 +290,9 @@ function setup() {
     });
 
     // Create the "Speed" slider
-    speedSlider = createSlider(1, 10, speed, 1);
+    speedSlider = createSlider(SPEED_MIN, SPEED_MAX, speed, SPEED_STEP);
     speed = speedSlider.value();
-    speedSlider.position(1450, 250);
+    speedSlider.position(SLIDER_X_POSITION, 250);
     speedSlider.size(200);
     // function to change the speed of the particles based on the slider value
     speedSlider.input(() => {
@@ -273,14 +320,9 @@ function draw() {
         grid[i].show();
     }
 
-    elapsedTime =
-        Math.floor(time / 3600).toString().padStart(2, "0") + ":" +
-        Math.floor((time % 3600) / 60).toString().padStart(2, "0") + ":" +
-        Math.floor((time % 60)).toString().padStart(2, "0");
-
     document.getElementById("time-heading").textContent = "ELAPSED TIME: " + elapsedTime;
 
-    
+
     // display the particles 
     // if condition to check if start button is pressed
     for (var i = 0; i < particles.length; i++) {
@@ -290,7 +332,7 @@ function draw() {
             particles[i].handleCollision();
         }
     }
-    
+
     checkMazeCompletion(); // Check if any particle has reached the last cell
 
 }
@@ -355,30 +397,28 @@ function generateNewMaze() {
     resetButton.attribute('disabled', true);
 
     // Create cells and add them to the grid
-    for (var row = 0; row < rows; row++) {
-        for (var column = 0; column < cols; column++) {
-            var cell = new Cell(column, row);
-            grid.push(cell);
-        }
-    }
+    createCells();
 
     // code for timer 
     stopTimer(); // Stop and reset the timer
     time = 0; // Reset the time to zero
     elapsedTime = "00:00:00";
 
-
     generateMaze();
     particles.splice(0, particles.length); // Remove all particles
 }
 
+// THE FOLLOWING FUNCTIONS ARE FOR THE START, STOP, AND RESET BUTTONS 
+
 // Function to start the maze
 function startMaze() {
     // setting up the initialization of the particles 
-    for (var i = 0; i < noOfParticles; i++) {
-        let particle = new Particle();
-        particle.velocity.mult(speed);
-        particles.push(particle);
+    if (particles.length === 0) {
+        for (var i = 0; i < noOfParticles; i++) {
+            let particle = new Particle();
+            particle.velocity.mult(speed);
+            particles.push(particle);
+        }
     }
 
     startButtonPressed = true;
@@ -424,56 +464,5 @@ function resetMaze() {
         stopTimer(); // Stop and reset the timer
         time = 0; // Reset the time to zero
         elapsedTime = "00:00:00";
-    }
-}
-
-class Particle {
-    constructor() {
-        this.radius = 10;
-
-        // the ball will appear in the first cell, however, it will be random between 1 - 50 at what pixel it appears 
-        this.position = createVector(grid[0].i * size + random(size - 10), grid[0].j * size + random(size - 10)); // position of the particles, will always start with the first cell
-        this.velocity = p5.Vector.random2D(); // new 2D unit vector with a random heading.
-    }
-
-    show() { // style of the ellipse
-        stroke(0, 150);
-        fill(112, 183, 126);
-        ellipse(this.position.x, this.position.y, this.radius);
-    }
-
-    move() {
-        this.position.add(this.velocity); // continuously adds the change in position to the position vector 
-    }
-
-    handleCollision() {
-        for (var gridElement = 0; gridElement < grid.length; gridElement++) {
-            var cell = grid[gridElement];
-            if (this.position.x > cell.i * size && this.position.x < (cell.i + 1) * size &&
-                this.position.y > cell.j * size && this.position.y < (cell.j + 1) * size) {
-                // Particle is inside the cell
-                if (cell.walls[0] && this.position.y - this.radius < cell.j * size) {
-                    // Top wall collision
-                    this.velocity.y *= -1; // Reverse the y velocity
-                    this.position.y = cell.j * size + this.radius; // Adjust position to prevent overlap
-                }
-                if (cell.walls[1] && this.position.x + this.radius > (cell.i + 1) * size) {
-                    // Right wall collision
-                    this.velocity.x *= -1; // Reverse the x velocity
-                    this.position.x = (cell.i + 1) * size - this.radius; // Adjust position to prevent overlap
-                }
-                if (cell.walls[2] && this.position.y + this.radius > (cell.j + 1) * size) {
-                    // Bottom wall collision
-                    this.velocity.y *= -1; // Reverse the y velocity
-                    this.position.y = (cell.j + 1) * size - this.radius; // Adjust position to prevent overlap
-                }
-                if (cell.walls[3] && this.position.x - this.radius < cell.i * size) {
-                    // Left wall collision
-                    this.velocity.x *= -1; // Reverse the x velocity
-                    this.position.x = cell.i * size + this.radius; // Adjust position to prevent overlap
-                }
-            }
-        }
-
     }
 }
