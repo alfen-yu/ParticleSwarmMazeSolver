27e3a2b4264374971b5ee59ccd7b38a9bf059cb2
Yousuf Uyghur <yousufuighur@yahoo.com>
2024-04-02 19:14:30 +0500

    added elasped time and fps

--- PSOMaze.js
+++ PSOMaze.js
@@ -7,21 +7,93 @@
 // Invoke the routine recursively for the chosen cell
 // which is invoked once for any initial cell in the area.
 
+// CONSTANTS 
+const CANVAS_WIDTH = 1000;
+const CANVAS_HEIGHT = 800;
+const BUTTON_X_POSITION = 1150;
+const BUTTON_Y_POSITIONS = [200, 250, 300, 350];
+const SLIDER_X_POSITION = 1450;
 
-let cols, rows; 
+const PARTICLE_MIN = 300;
+const PARTICLE_MAX = 10000;
+const PARTICLE_STEP = 50;
+const SPEED_MIN = 1;
+const SPEED_MAX = 10;
+const SPEED_STEP = 1;
+
+let cols, rows;
 const size = 50; // Size of each cell in the grid 
 var grid = []; // Array to hold the cells of the grid 
-let current; 
-let last; 
+let current;
 let stack = [];
-const particles = []; 
-let noOfParticles = 1000; 
+const particles = [];
+let noOfParticles = 1000;
 let speed = 1;
+let last; 
 // buttons and sliders
 let newMazeButton, startButton, pauseButton, resetButton;
 let particleSlider, speedSlider;
 let startButtonPressed = false;
 let elapsedTime = "00:00:00";
+let time = 0;
+let interval;
+
+function styleButton(button) {
+    button.style('background-color', '#4CAF50');
+    button.style('padding', '10px 20px');
+    button.style('font-size', '16px');
+    button.style('border', 'none');
+    button.style('border-radius', '5px');
+}
+
+function toggleButtonAttribute(button, attribute, value) {
+    if (value) {
+        button.removeAttribute(attribute);
+    } else {
+        button.attribute(attribute, true);
+    }
+}
+
+// Function to check if any particle has reached the last cell
+function checkMazeCompletion() {
+    for (var i = 0; i < particles.length; i++) {
+        var particle = particles[i];
+        // Calculate the position of the particle in terms of grid cells
+        var particleColumn = Math.floor(particle.position.x / size);
+        var particleRow = Math.floor(particle.position.y / size);
+        
+        // Check if the particle has reached the last cell
+        if (particleColumn === cols - 1 && particleRow === rows - 1) {
+            pauseMaze(); // Pause the maze
+            stopTimer(); // Stop the timer
+            var completionTime = elapsedTime;
+            var completionParticles = particles.length;
+            var completionSpeed = speed;
+            alert("Maze completed!\nTime: " + completionTime + "\nParticles: " + completionParticles + "\nSpeed: " + completionSpeed + "\nRefresh to Restart.");
+            break;
+        }
+    }
+}
+
+// function to start the stopwatch 
+function startTimer() {
+    if (interval) {
+        clearInterval(interval);
+    }
+
+    // sets the time 
+    interval = setInterval(() => {
+        time += 1
+        elapsedTime =
+            Math.floor(time / 3600).toString().padStart(2, "0") + ":" + Math.floor((time % 3600) / 60).toString().padStart(2, "0") + ":" + Math.floor((time % 60)).toString().padStart(2, "0")
+    }, 1000);
+}
+
+// stops and clear the timer 
+function stopTimer() {
+    clearInterval(interval);
+    interval = null;
+}
 
 // Function to calculate the index of a cell in the grid array
 function index(column, row) {
@@ -41,7 +113,7 @@ class Cell {
         this.j = j;
 
         this.walls = [true, true, true, true];
-        
+
         this.visited = false;
     }
 
@@ -62,8 +134,8 @@ class Cell {
             noStroke();
             fill(206, 20, 131, 100);
             rect(x, y, size, size);
-        } 
-        
+        }
+
     }
 
     // Method to check neighboring cells and return unvisited neighbors
@@ -114,13 +186,13 @@ function removeWall(current, neighbour) {
 
 // Setup function to initialize the canvas and grid
 function setup() {
-    createCanvas(1000, 800);
+    createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
     cols = floor(width / size);
     rows = floor(height / size);
-    
+
     frameRate(120);
 
-    document.getElementById("time-heading").textContent = "ELAPSED TIME: " + elapsedTime; 
+    document.getElementById("time-heading").textContent = "ELAPSED TIME: " + elapsedTime;
 
     // Create cells and add them to the grid
     for (var row = 0; row < rows; row++) {
@@ -132,48 +204,32 @@ function setup() {
 
     generateMaze();
 
-    
+
     // Create the "Generate New Maze" button
     newMazeButton = createButton('Generate New Maze');
     newMazeButton.position(1150, 200);
     newMazeButton.elt.addEventListener('click', generateNewMaze);
-    newMazeButton.style('background-color', '#4CAF50');
-    newMazeButton.style('padding', '10px 20px');
-    newMazeButton.style('font-size', '16px');
-    newMazeButton.style('border', 'none');
-    newMazeButton.style('border-radius', '5px');
+    styleButton(newMazeButton);
 
     // Create the "Start" button
     startButton = createButton("Start the Maze");
     startButton.position(1150, 250);
     startButton.elt.addEventListener('click', startMaze);
-    startButton.style('background-color', '#4CAF50');
-    startButton.style('padding', '10px 20px');
-    startButton.style('font-size', '16px');
-    startButton.style('border', 'none');
-    startButton.style('border-radius', '5px');
+    styleButton(startButton);
 
     // Create the "Pause" button
     pauseButton = createButton("Pause the Maze");
     pauseButton.position(1150, 300);
     pauseButton.elt.addEventListener('click', pauseMaze);
     pauseButton.attribute('disabled', true);
-    pauseButton.style('background-color', '#4CAF50');
-    pauseButton.style('padding', '10px 20px');
-    pauseButton.style('font-size', '16px');
-    pauseButton.style('border', 'none');
-    pauseButton.style('border-radius', '5px');
+    styleButton(pauseButton);
 
     // Create the "Stop" button
     resetButton = createButton("Reset the Maze");
     resetButton.position(1150, 350);
     resetButton.elt.addEventListener('click', resetMaze);
     resetButton.attribute('disabled', true);
-    resetButton.style('background-color', '#4CAF50');
-    resetButton.style('padding', '10px 20px');
-    resetButton.style('font-size', '16px');
-    resetButton.style('border', 'none');
-    resetButton.style('border-radius', '5px');
+    styleButton(resetButton);
 
     // paramters: min, max, initial value, step
     // Create the "Number of Particles" slider
@@ -201,28 +257,42 @@ function setup() {
 // Draw function to continuously update and display the grid
 function draw() {
     background(40);
-    
+
     // Values of the Sliders
     document.getElementById("particle-text").textContent = "Particles: " + particleSlider.value();
     document.getElementById("speed-text").textContent = "Speed: " + speedSlider.value();
 
+    // Calculate and display FPS every 1 second
+    if (frameCount % 60 === 0) {
+        const fps = Math.floor(frameRate());
+        document.getElementById("frame-rate").textContent = "FPS: " + fps;
+    }
 
     // Display each cell in the grid
     for (var i = 0; i < grid.length; i++) {
         grid[i].show();
     }
 
+    elapsedTime =
+        Math.floor(time / 3600).toString().padStart(2, "0") + ":" +
+        Math.floor((time % 3600) / 60).toString().padStart(2, "0") + ":" +
+        Math.floor((time % 60)).toString().padStart(2, "0");
+
+    document.getElementById("time-heading").textContent = "ELAPSED TIME: " + elapsedTime;
+
+    
     // display the particles 
     // if condition to check if start button is pressed
-    for (var i = 0; i < particles.length; i++){
+    for (var i = 0; i < particles.length; i++) {
         particles[i].show();
         if (startButtonPressed) {
             particles[i].move();
             particles[i].handleCollision();
         }
     }
-
     
+    checkMazeCompletion(); // Check if any particle has reached the last cell
+
 }
 
 // function that generates the maze instantly instead of backtracking every cell, as seen previously
@@ -263,7 +333,7 @@ function generateMaze() {
             break;
         }
     }
-}  
+}
 
 // Function to generate new maze
 function generateNewMaze() {
@@ -292,6 +362,12 @@ function generateNewMaze() {
         }
     }
 
+    // code for timer 
+    stopTimer(); // Stop and reset the timer
+    time = 0; // Reset the time to zero
+    elapsedTime = "00:00:00";
+
+
     generateMaze();
     particles.splice(0, particles.length); // Remove all particles
 }
@@ -307,9 +383,11 @@ function startMaze() {
 
     startButtonPressed = true;
 
+    startTimer();
+
     // disable all these buttons and sliders
     startButton.attribute('disabled', true);
-    speedSlider.attribute('disabled', 'true'); 
+    speedSlider.attribute('disabled', 'true');
     particleSlider.attribute('disabled', 'true');
 
     // enable stop and reset buttons
@@ -326,6 +404,7 @@ function pauseMaze() {
     startButtonPressed = false;
     startButton.removeAttribute('disabled');
     pauseButton.attribute('disabled', true);
+    stopTimer(); // stops the timer 
 }
 
 // Function to reset the maze
@@ -334,17 +413,17 @@ function resetMaze() {
         startButtonPressed = false;
         startButton.removeAttribute('disabled');
         // removing previous and creating new particles
-        particles.splice(0, particles.length); 
-        for (var i = 0; i < noOfParticles; i++) {
-            let particle = new Particle();
-            particles.push(particle);
-        }
+        particles.splice(0, particles.length);
+
         // disable the pause and reset buttons
         resetButton.attribute('disabled', true);
         pauseButton.attribute('disabled', true);
         // enable the sliders
-        speedSlider.removeAttribute('disabled'); 
+        speedSlider.removeAttribute('disabled');
         particleSlider.removeAttribute('disabled');
+        stopTimer(); // Stop and reset the timer
+        time = 0; // Reset the time to zero
+        elapsedTime = "00:00:00";
     }
 }
 
@@ -353,7 +432,7 @@ class Particle {
         this.radius = 10;
 
         // the ball will appear in the first cell, however, it will be random between 1 - 50 at what pixel it appears 
-        this.position = createVector(grid[0].i * size + random(size - 10), grid[0].j * size + random(size- 10)); // position of the particles, will always start with the first cell
+        this.position = createVector(grid[0].i * size + random(size - 10), grid[0].j * size + random(size - 10)); // position of the particles, will always start with the first cell
         this.velocity = p5.Vector.random2D(); // new 2D unit vector with a random heading.
     }
 
@@ -395,6 +474,6 @@ class Particle {
                 }
             }
         }
-        
+
     }
 }
